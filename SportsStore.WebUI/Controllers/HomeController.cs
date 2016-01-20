a75bb984216980e74e8d5e45b8e5bc25d1eb2c81
Alexander <m.alexander.1990@yandex.ua>
2016-01-20 19:53:54 +0200

    SportsStore

---
+++ SportsStore.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.MyMessageToUsers = "Hello from me";
+            ViewBag.AnswerText = "Your Answers goes here";
+      
+            return View();
+        }
+    }
+}
